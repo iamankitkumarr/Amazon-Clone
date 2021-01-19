b3a1a6ac3703412657f385f24cc9e9e211d2482d
Ankit Kumar <53473862+iamankitkumarr@users.noreply.github.com>
2021-01-19 19:44:03 +0530

    Add files via upload

---
+++ amazon_clone-master/src/firebase.js
@@ -0,0 +1,9 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+const firebaseConfig = {
+    //paste your own firebase config
+  };
+  const firebaseApp=firebase.initializeApp(firebaseConfig);
+  const db=firebaseApp.firestore();
+  const auth=firebase.auth();
+  export {db,auth};
